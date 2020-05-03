244134ca64d1aeeb68f59ea21eefd2c98329964b
Javier Giner Álvarez <jginera@indra.es>
2020-05-03 20:55:44 +0200

    Initial Commit

---
+++ personalRecipes/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  personalRecipes
+//
+//  Created by Javier Giner Alvarez on 03/05/2020.
+//  Copyright © 2020 jginerafatimagc.com. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ personalRecipes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  personalRecipes
+//
+//  Created by Javier Giner Alvarez on 03/05/2020.
+//  Copyright © 2020 jginerafatimagc.com. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
